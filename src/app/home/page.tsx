f2267eb31f44c60429e5e2c0f72ce6e423e104ce
danylokarpenko <karpenkodanylo@gmail.com>
2024-04-05 15:40:47 +0300

    move login btn to main page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import { Link } from '../components/ui/link/Link';
 export default function AuthPage() {
   return (
     <div className="flex justify-center">
-      <Link href="/auth">BACK to auth page</Link>
+      <Link href="/">BACK to auth page</Link>
     </div>
   );
 }
